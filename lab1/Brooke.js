8bb0a637c54a88e2edb0ca0903214c20119a967c
BrookeEden <eden.brooke99@yahoo.com>
2021-01-22 14:16:53 -0500

    Lab1 done

---
+++ lab1/Brooke.js
@@ -0,0 +1,8 @@
+import Person from './Person.js';
+
+class Brooke extends Person{
+  constructor(){
+    super("Brooke", "#800080");
+  }
+}
+export default Brooke;
---
+++ lab1/index.js
@@ -0,0 +1,4 @@
+import Brooke from './Brooke.js';
+
+const p = new Brooke("Brooke", "#800080");
+p.speak();
