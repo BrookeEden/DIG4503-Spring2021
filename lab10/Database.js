e0f4c19a2a4768e3069bce6f2f6fcd88e99824c5
BrookeEden <eden.brooke99@yahoo.com>
2021-04-03 21:42:53 -0400

    lab10 done

---
+++ lab10/Database.js
@@ -0,0 +1,42 @@
+import MongoClient from 'mongodb';
+
+const URL = "mongodb+srv://BrookeEden:6ZShvtTHyHjMx0zB@cluster0.yuzwq.mongodb.net";
+
+class Database {
+  constructor() {
+      this.connection = null;
+      this.database = null;
+      this.collection = null;
+  }
+  async connect() {
+      this.connection = await MongoClient.connect(url, { useUnifiedTopology: true });
+      this.database = this.connection.db("lab10");
+      this.collection = this.database.collection("people"); 
+  }
+      async createOne(first, last, color) {
+          let object = {
+              "firstName": first,
+              "lastName": last,
+              "favoriteColor": color
+          };
+          if(this.collection != null) {
+              return await this.collection.insertOne(object);
+          }
+      }
+  async readOne(query) {
+      let results = await this.collection.findOne({firstName: query});
+          if (results != null) {
+              return results;
+          }
+          else{
+              return{person: "not found"};
+          }
+  }
+  close() {
+  if(this.collection != null) {
+    this.connection.close();
+    }
+  }
+}
+
+export default Database;
