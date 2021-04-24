f8d10a95e0e5024fe534a984a2bbdf5544353d1b
BrookeEden <eden.brooke99@yahoo.com>
2021-04-24 12:49:13 -0400

    i think i can i think i can

---
+++ lab12/server/Database.js
@@ -0,0 +1,77 @@
+import MongoClient from 'mongodb';
+
+const URL = "mongodb+srv://BrookeEden:6ZShvtTHyHjMx0zB@cluster0.yuzwq.mongodb.net";
+
+//MAKE SKELTON FIRST OF WHAT NEEDS TO BE DONE!!!!
+
+class Database {
+    constructor() {
+      this.collection = null;
+      this.database = null;
+      this.connection = null;
+  }
+//async MUST have await to work!!
+    async connect(database, collection) {
+      this.connection = await MongoClient.connect(URL);
+      this.database = this.connection.db(database);
+      this.collection = this.database.collection(collection);
+    }
+    
+    async createOne(ISBN, title, author, description) {
+      if (this.collection != null) {
+        const result = await this.collection.insertOne({"ISBN": ISBN, "title": title, "author": author, "description": description});
+            return {"ISBN": ISBN, "title": title, "author": author, "description": description};
+        }
+      else {
+            return null;
+      }
+    }
+    async readOne(ISBN) {
+      if (this.collection !=null) {
+        const result = await this.collection.findOne( {
+          "ISBN": ISBN});
+            return result;
+      }
+      else {
+            return{books: "not found"};
+      }
+    }
+    async readMany(title, author) {
+        if (this.connection !=null) {
+          const result = await this.collection.findMany( {
+            "title":title,
+            "author": author});
+             // return{"title": title, "author": author};
+             return result;
+          }
+        else {
+            return{books: "not found"};
+        }    
+      }
+// do we add ISBN to the ()?
+    async updateOne(title, author, description) {
+        if(this.collection != null) {
+          const result = await this.collection.updateOne({"ISBN": ISBN}, 
+          {$set: {"title": title}, "author": author, "description": description});
+            return{"title": title, "author": author, "description":description}; 
+        } 
+        else {
+            return null;
+        }
+    }
+    async deleteOne(ISBN) {
+        if (this.collection != null) {
+          const result = await this.collection.deleteOne({"ISBN":ISBN});
+            return {"deleted": result.deletedCount};
+        }
+        else {
+            return{"deleted":0};
+           }
+    }  
+close() { 
+  if(this.collection !=null ) {
+    this.collection.close();
+    }
+  }
+};
+export default Database;
---
+++ lab12/server/index.js
@@ -0,0 +1,70 @@
+import Express from "express";
+import CORS from "cors";
+import Database from "./Database.js";
+
+const App = Express();
+const port = 45040; //changed port!!!
+
+App.use(Express.json()); //allows use request bodies & translate it out of JSON
+App.use(CORS());
+
+const db= new Database();
+
+db.connect("lab11", "books");
+
+//MAKE SKELTON FIRST OF WHAT NEEDS TO BE DONE!!!!
+//PUT -> createOne() -> collection.insertOne()
+App.put("/books/:ISBN", async (req, res) => {
+  const ISBN= req.params.ISBN;
+  const title= req.body.title;
+  const author= req.body.author;
+  const description= req.body.description;
+
+  const result = await db.createOne(ISBN, title, author, description);
+    res.json(result);
+    /*res.json ({
+      ISBN: ISBN,
+      title: title,
+      author: author,
+      description: description,
+    });*/
+});
+//GET -> readOne() -> collection.findOne()
+App.get("/books/:ISBN", async (req, res) => {
+    const ISBN = req.params.ISBN;
+    const result = await db.readOne(ISBN)
+      res.json(result);
+});
+//POST -> readMany() -> collection.findMany()
+App.post ("/books/:search", async (req, res) => {
+    const title= req.query.title;
+    const author= req.query.author; 
+    const result = await db.readMany(title, author);
+    res.json({
+        URLSearchParams: req.query.result
+    })
+    return result;
+});
+//PATCH -> updateOne() -> collection.updateOne()
+App.patch ("/books/:ISBN", async (req, res) => {
+  const ISBN= req.params.ISBN;
+  const title= req.body.title;
+  const author= req.body.author;
+  const description= req.body.description;
+    /*res.json ({
+      ISBN: ISBN,
+      title: title,
+      author: author,
+      description: description,
+    });*/
+  const result = await db.updateOne(title,author,description);
+    res.json(result); 
+});
+//DELETE -> deleteOne() -> collection.deleteOne()
+App.delete ("/books/:ISBN", async (req, res) => {
+  const ISBN = req.params.ISBN;
+  const result = await db.deleteOne(ISBN);
+    res.json(result);
+});
+
+App.listen(port);
